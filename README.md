d830c84f15d09c2c277c18d682d6f346b5741dcf
Chris <chrisdominguez2001@gmail.com>
2023-03-17 19:09:13 -0400

    Added template

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Banking-app
+To put all installed packeges in a file use: pip freeze > filename.txt
---
+++ prj4/bank/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('')
+
+def about(request):
+    return HttpResponse('')
+
+def login(request):
+    return HttpResponse('')
+
+def signup(request):
+    return HttpResponse('')
+
